d8845854e5e2fa4326013834af808f504a398d80
Cody Bender <cody.bender@pdq.com>
2024-07-23 13:47:03 -0400

    chore: fix color maybe

--- src/components/dle-card.jsx
+++ src/components/dle-card.jsx
@@ -12,24 +12,23 @@ export const DleCard = ({
     setState((prev) => ({ ...prev, [title]: true }));
   };
 
+  const color = secondary ? "text-sapphire" : "text-rosewater";
   return (
-    <div>
+    <>
       <a
-        class="md:w-1/2 p-4 flex flex-col hover:bg-surface0"
+        class={`md:w-1/2 p-4 flex flex-col hover:bg-surface0 `}
         href={link}
         target="_blank"
         onClick={handleClick}
       >
         <div class="flex">
-          <p class={`text-md text-${secondary ? "sapphire" : "rosewater"}`}>
-            {title}
-          </p>
+          <span class={`text-md ${color}`}>{title}</span>
           <Show when={state()[title]}>
             <span class="ml-2">✅</span>
           </Show>
         </div>
         <p class="text-sm text-subtext1">{description}</p>
       </a>
-    </div>
+    </>
   );
 };
